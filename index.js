c1f9bc0ef04a9bf381542c8a4b87601661527744
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2024-06-02 22:46:46 +0200

    backend

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());

dbf29e065d015e4b3626f4fad7d1f3b24abf73cc
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2024-06-20 14:52:32 +0200

    deploy config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 // api/index.js
-const serverless = require('serverless-http');
 const app = require('../index'); // Adjust path if your server.js is in a different location
 
 module.exports = app;  // For local testing
-module.exports.handler = serverless(app);  //

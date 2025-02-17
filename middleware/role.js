6617aae97c62fd65ecf6d3b7f4410003c6bbb5a5
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2025-02-17 13:58:08 +0100

    fix(index): update

--- middleware/role.js
+++ middleware/role.js
@@ -1,8 +1,8 @@
 module.exports = (role) => {
-    return (req, res, next) => {
-        if (req.user.role !== role) {
-            return res.status(403).json({ status: 'error', error: 'Just ADMIN can See And Update' });
-        }
-        next();
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).json({status: "error", error: "Just ADMIN can See And Update"})
     }
-};
+    next()
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const {signup, login, userAddPoduct, userRemovePoduct, UserListCartDate, charge, test} = require("../controllers/usersController");
-const authorize = require("../middleware/authorize");
-const router = express.Router();
+const express = require("express")
+const {signup, login, userAddPoduct, userRemovePoduct, UserListCartDate, charge, test} = require("../controllers/usersController")
+const authorize = require("../middleware/authorize")
+const router = express.Router()
 
-router.post("/signup", signup);
+router.post("/signup", signup)
 
-router.post("/login", login);
+router.post("/login", login)
 
-router.post("/userAddPoduct", authorize, userAddPoduct);
+router.post("/userAddPoduct", authorize, userAddPoduct)
 
-router.post("/userRemovePoduct", authorize, userRemovePoduct);
+router.post("/userRemovePoduct", authorize, userRemovePoduct)
 
-router.post("/UserListCartDate", authorize, UserListCartDate);
+router.post("/UserListCartDate", authorize, UserListCartDate)
 
-router.post('/charge', authorize, charge);
+router.post("/charge", authorize, charge)
 
-module.exports = router;
+module.exports = router
